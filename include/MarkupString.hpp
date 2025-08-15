31d77e850506f1a3c068f7aa0fdde405f4507645
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2025-08-15 20:44:31 +0200

    Bump and warning mittigation

--- include/MarkupString.hpp
+++ include/MarkupString.hpp
@@ -11,7 +11,7 @@ public:
 	explicit MarkupString(const wxString& str) : wxString(str) {}
 
 	MarkupString() {};
-	MarkupString(wxWindow* window);;
+	MarkupString(wxWindow* window);
 
 	MarkupString& EndInharitance();
 
